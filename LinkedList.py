e0ea89802e92a1d3f19fda02cafac9e4bda6fde7
LetTheBassKick <royzhang42@gmail.com>
2019-06-10 16:05:15 -0700

    sample code

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,13 @@
 # Linked List implementation
+
+class Node():
+    def __init__(self, data, next, prev):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class LinkedList():
+    def __init__(self, head, tail):
+        self.head = head
+        self.tail = tail
+
