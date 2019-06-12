e6b859f5b1b5533f977bad78dc17a46cc3e0bdec
LetTheBassKick <royzhang42@gmail.com>
2019-06-12 11:08:46 -0700

    Linked List stuff

---
+++ Heap.py
@@ -0,0 +1,6 @@
+# Heap Implementaiton
+
+class Heap():
+
+    def __init__(self, array = None):
+        pass
--- LinkedList.py
+++ LinkedList.py
@@ -1,13 +1,40 @@
 # Linked List implementation
 
 class Node():
-    def __init__(self, data, next, prev):
+    def __init__(self, data):
         self.data = data
-        self.next = next
-        self.prev = prev
+        self.next = None
+        self.prev = None
+
 
 class LinkedList():
-    def __init__(self, head, tail):
-        self.head = head
-        self.tail = tail
+    def __init__(self, array = None):
+        self.head = None
+        self.tail = None
 
+        if array != None:
+            for num in array:
+                self.append(num)
+        pass
+
+    def append(self, insert):
+        insert_node = Node(insert)
+        if self.head == None:
+            self.head = insert_node
+            self.tail = insert_node
+        else:
+            self.tail.next = insert_node
+            insert_node.prev = self.tail
+            self.tail = insert_node
+        pass
+    
+    def display(self):
+        curr = self.head
+        while curr != None:
+            print(curr.data)
+            curr = curr.next
+
+sample = Node(1)
+sample_list = LinkedList([1,2,3])
+
+sample_list.display()
